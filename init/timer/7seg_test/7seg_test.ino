4c84d27cfc42d3a419a3989f3b237896c45f6fd6
jheon1223 <jheon1223@gmail.com>
2025-07-21 15:45:33 +0900

    add player2 7segment-4digit

--- init/timer/7seg_test/7seg_test.ino
+++ init/timer/7seg_test/7seg_test.ino
@@ -1,67 +1,55 @@
-int digit_select_pin[] = {1,2,3,4}; //7seg 선택핀
-int segment_pin[] = {5,6,7,8,9,10,11,12}; //7seg 제어핀
-int time_delay = 5;
-byte digits_data[10]={0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xE6};
-int d1000, d100, d10, d1;
+#include <TM1637Display.h>
 
-void setup()
-{
-  //자릿수 선택 핀을 출력으로 설정
-  for(int i = 0; i < 4; i++)
-  {
-    pinMode(digit_select_pin[i], OUTPUT);
-  }
-  // 세그먼트 제어 핀을 풀력으로 설정
-  for(int i = 0; i < 8; i++)
-  {
-    pinMode(segment_pin[i], OUTPUT);
-  }
+// Player1 핀 연결
+int digit_select_pin[] = {1, 2, 3, 4};     // digit 선택
+int segment_pin[] = {5, 6, 7, 8, 9, 10, 11, 12}; // a ~ dp
+byte digits_data[10] = {0xFC, 0x60, 0xDA, 0xF2,
+                        0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xE6};
+int time_delay = 2;
+
+// Player2 핀 (TM1637)
+#define CLK A1
+#define DIO A0
+TM1637Display display(CLK, DIO);
+
+void setup() {
+  // Player1 핀 설정
+  for (int i = 0; i < 4; i++) pinMode(digit_select_pin[i], OUTPUT);
+  for (int i = 0; i < 8; i++) pinMode(segment_pin[i], OUTPUT);
+
+  // Player2 디스플레이 밝기 설정
+  display.setBrightness(1); // 0~7
 }
-//해당 자릿수에 숫자 하나를 표시하는 함수
-//(위치, 출력할 숫자)
-void show_digit(int pos, int number)
-{
-  for(int i = 0; i < 4; i++)
-  {
-    if(i+1 ==pos)
-      //해당 자릿수의 선택 핀만 LOW로 설정
+
+void show_digit(int pos, int number) {
+  for (int i = 0; i < 4; i++) {
+    if (i + 1 == pos)
       digitalWrite(digit_select_pin[i], HIGH);
     else
-      //나머지 자리는 HIGH로 설정
       digitalWrite(digit_select_pin[i], LOW);
   }
-  // 8개 세그먼트 제어
-  for(int i = 0; i < 8; i++)
-  {
-    byte segment_data = (digits_data[number] 
-         & (0x01 << i)) >> i;
-    if(segment_data == 1)
-      digitalWrite(segment_pin[7-i], LOW);
-    else
-      digitalWrite(segment_pin[7-i], HIGH);
+  for (int i = 0; i < 8; i++) {
+    byte segment_data = (digits_data[number] & (0x01 << i)) >> i;
+    digitalWrite(segment_pin[7 - i], segment_data ? LOW : HIGH);
   }
 }
 
-void loop()
-{
-  for( int i = 0; i < 10000; i++)
-  {
- 
-    d1000 = i /1000;
-    d100 = i % 1000/100;
-    d10 = i % 100/10;
-    d1 = i % 10;
-    for(int j = 0 ; j < 10 ; j++)
-    {
- 
-     show_digit(1, d1000);
-     delay(time_delay);
-     show_digit(2, d100);
-     delay(time_delay);
-     show_digit(3, d10);
-     delay(time_delay);
-     show_digit(4, d1);
-     delay(time_delay);
+void loop() {
+  for (int i = 0; i < 10000; i++) {
+    int d1000 = i / 1000;
+    int d100 = (i % 1000) / 100;
+    int d10 = (i % 100) / 10;
+    int d1 = i % 10;
+
+    for (int j = 0; j < 10; j++) {
+      // Player1 표시
+      show_digit(1, d1000); delay(time_delay);
+      show_digit(2, d100);  delay(time_delay);
+      show_digit(3, d10);   delay(time_delay);
+      show_digit(4, d1);    delay(time_delay);
+
+      // Player2 표시
+      display.showNumberDec(i, true); // TM1637은 알아서 표시됨
     }
   }
 }
